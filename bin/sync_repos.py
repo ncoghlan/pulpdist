d7314387e0e26e627fca3457535461fb1982c172
Nick Coghlan <ncoghlan@redhat.com>
2011-12-06 07:58:20 +1000

    Update sync_repos.py CLI to sync all repos if no repo list is provided (and to use a proper argument parser)

--- bin/sync_repos.py
+++ bin/sync_repos.py
@@ -12,16 +12,51 @@
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.\
 
+import argparse
 import json
+
+try:
+    import pulpdist
+except ImportError:
+    # Allow running from source checkout
+    import sys
+    import os.path
+    _this = os.path.dirname(os.path.abspath(__file__))
+    _src_dir = os.path.normpath(os.path.join(_this, "..", "src"))
+    sys.path.insert(0, _src_dir)
+
 from pulpdist.core.pulpapi import PulpServerClient
+
+def _make_parser():
+    description="Synchronise Pulp Repositories"
+    epilog = ("The expected JSON file format is a top level list containing "
+              "mappings with a 'repo_id' attribute. Any other fields are "
+              "ignored.")
+    parser = argparse.ArgumentParser(description=description, epilog=epilog)
+    parser.add_argument("pulp_host", metavar="HOST", type=str,
+                    help="The Pulp server with the repos to be synchronised")
+    parser.add_argument("repo_fname", metavar="REPO_LIST", type=str, nargs="?",
+                    help="A JSON file identifying repos to synchronise")
+    return parser
+
+def _main(argv):
+    args = _make_parser().parse_args(argv)
+    # Must have already saved credentials with "pulp-admin auth login"
+    pulp_host = args.pulp_host
+    server = PulpServerClient(pulp_host)
+    repo_fname = args.repo_fname
+    if repo_fname:
+        with open(repo_fname) as repo_file:
+            print("Reading repository list from {0!r}".format(repo_fname))
+            repo_list = [repo["repo_id"] for repo in json.load(repo_file)]
+    else:
+        print("Retrieving repository list from {0!r}".format(pulp_host))
+        repo_list = [repo["id"] for repo in server.get_repos()]
+    for repo_id in repo_list:
+        print("Syncing {0}".format(repo_id))
+        server.sync_repo(repo_id)
+
 
 if __name__ == "__main__":
     import sys
-    pulp_host, repo_fname = sys.argv[1:]
-    # Must have already saved credentials with "pulp-admin auth login"
-    server = PulpServerClient(pulp_host)
-    with open(repo_fname) as repo_file:
-        repo_list = json.load(repo_file)
-    for repo_details in repo_list:
-        repo_id = repo_details["repo_id"]
-        server.sync_repo(repo_id)
+    _main(sys.argv[1:])
