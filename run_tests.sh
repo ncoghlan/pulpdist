f399fd42dd50177806f52b2f6cbd465d1b4ce25b
Nick Coghlan <ncoghlan@redhat.com>
2012-03-06 16:57:17 +1000

    Ensure additional tests that nose doesn't pick up are still executed

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,5 @@
 pushd src/
-python -m unittest discover -v -t . -s pulpdist/core -s pulpdist/pulp_plugins
+python -m unittest discover -v -t . -s pulpdist/core
+python -m unittest discover -v -t . -s pulpdist/pulp_plugins
 pulpdist/manage_site.py test $*
 popd
