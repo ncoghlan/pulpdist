e7d6355128b1c9cf182da0496ea394c291181034
Nick Coghlan <ncoghlan@redhat.com>
2012-03-09 17:16:50 +1000

    Correct file title

--- src/pulpdist/core/tests/example_trees.py
+++ src/pulpdist/core/tests/example_trees.py
@@ -11,7 +11,8 @@
 # NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
-"""Basic test suite for sync transfer operations"""
+
+"""Tree definitions for use in testing"""
 
 import shutil
 import tempfile
