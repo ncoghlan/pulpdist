fcf52e3a2c538a543d04a2213d777cada1a124ce
Nick Coghlan <ncoghlan@redhat.com>
2012-04-11 13:26:41 +1000

    Update version number for 0.0.11 build

--- src/pulpdist/core/util.py
+++ src/pulpdist/core/util.py
@@ -15,7 +15,7 @@
 
 # This should be kept in sync with the version in the RPM spec file
 # The suffix should be removed before creating the RPM
-__version__ = "0.0.11a0"
+__version__ = "0.0.11"
 
 def format_iter(iterable, fmt='{0!r}', sep=', '):
     return sep.join(fmt.format(x) for x in iterable)
